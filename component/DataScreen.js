05420e78141cd6e5306618b916404386ab316502
yoyu <atp6u4m4a@gmail.com>
2022-02-16 11:48:04 +0800

    add activity indicator while fetching

--- component/DataScreen.js
+++ component/DataScreen.js
@@ -1,5 +1,5 @@
 import React, { useContext, useEffect, useState } from "react";
-import { View, Text, FlatList, StyleSheet } from "react-native";
+import { View, Text, FlatList, StyleSheet, ActivityIndicator } from "react-native";
 import { MainTitleUpdate } from '../navigator/Drawer';
 
 
